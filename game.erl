37312dd623f3dca6b41f526075b2cbccc7166e55
SARANKIRTHIC02 <sarankirthic1@gmail.com>
2024-06-20 01:21:24 -0400

    establish connection between master and process

--- game.erl
+++ game.erl
@@ -1,92 +1,40 @@
-% -module(game).
-% -export([start/1]).
 
-% start(Args) ->
-%     % Get the player file from the arguments
-%     PlayerFile = lists:nth(1, Args),
-%     {ok, PlayerInfo} = file:consult(PlayerFile),
-%     io:fwrite("Player info: ~p~n", [PlayerInfo]),
 
-%     MainMap = maps:from_list(PlayerInfo),
-%     io:fwrite("Main Map: ~p~n", [MainMap]),
-
-%     EligiblePlayersMap = create_eligible_players_map(MainMap, MainMap),
-%     lists:foreach(
-%         fun({Name, Credits}) ->
-%             Pid = spawn(player, player_listener, [Name,Credits, maps:get(Name, EligiblePlayersMap), maps:get(Name, MainMap)],self()),
-%             register(Name, Pid),
-%             Worker_pid = whereis(Name)
-%             % timer:sleep(200)
-%         end,
-%         PlayerInfo
-%     ),
-    
-%     %   lists:foreach(
-%     %     fun(Name) ->
-%     %         Current_Name = Name,
-%     %         io:fwrite("Main Map: ~p~n", [Name])
-%     %         Initiate = "Initiate",
-%     %         Current = whereis(Name),
-%     %         Current ! {message, {Current_Name, Initiate}}
-%     %     end,
-%     %     maps:keys(MainMap)
-%     % ).
-
-%         lists:foreach(
-%         fun(Name) ->
-%             % Current_Name = Name,
-%             io:fwrite("Current Player: ~p~n", [Name]),
-%             Initiate = "Initiate",
-%             Current = whereis(Name),
-%             Current ! {message, {Name, Initiate}}
-%         end,
-%         maps:keys(MainMap)
-%     ).
-
-% create_eligible_players_map(MainMap, EligiblePlayersMap) ->
-%     lists:foldl(
-%         fun({Player, _}, Acc) ->
-%             EligiblePlayers = lists:delete(Player, maps:keys(EligiblePlayersMap)),
-%             maps:put(Player, EligiblePlayers, Acc)
-%         end,
-%         #{},
-%         maps:to_list(MainMap)
-%     ).
 -module(game).
--export([start/1]).
+-export([start/1,master_listener/2,create_eligible_players_map/2]).
 
 start(Args) ->
-    % Get the player file from the arguments
     PlayerFile = lists:nth(1, Args),
     {ok, PlayerInfo} = file:consult(PlayerFile),
     io:fwrite("Player info: ~p~n", [PlayerInfo]),
 
+
     MainMap = maps:from_list(PlayerInfo),
     io:fwrite("Main Map: ~p~n", [MainMap]),
-
 
     EligiblePlayersMap = create_eligible_players_map(MainMap, MainMap),
     lists:foreach(
         fun({Name, Credits}) ->
             Pid = spawn(player, player_listener, [Name, Credits, maps:get(Name, EligiblePlayersMap), MainMap, self()]),
             register(Name, Pid),
-            Worker_pid = whereis(Name)
-            % io:format("Worker PID for ~p: ~p~n", [Name, Worker_pid])
+            io:format("Worker PID for ~p: ~p~n", [Name, whereis(Name)])
         end,
         PlayerInfo
     ),
-    % io:fwrite("Main Map: ~p~n", [EligiblePlayersMap]),
 
     lists:foreach(
         fun(Name) ->
-            % io:fwrite("Current Player: ~p~n", [Name]),
+            io:fwrite("Current Player: ~p~n", [Name]),
             Initiate = "Initiate",
             Current = whereis(Name),
             Current ! {message, {Name, Initiate}}
         end,
         maps:keys(MainMap)
-    ).
+    
+    ),
+    master_listener(MainMap,EligiblePlayersMap,0).
 
+    
 
 create_eligible_players_map(MainMap, EligiblePlayersMap) ->
     lists:foldl(
@@ -97,3 +45,16 @@ create_eligible_players_map(MainMap, EligiblePlayersMap) ->
         #{},
         maps:to_list(MainMap)
     ).
+
+master_listener(MainMap,EligiblePlayersMap, Game_id)->
+    % Record = {GameId:{sam:"scissor", jill:"paper"}}
+    receive
+        {create_game, Name, Random_Player} ->
+            New_gameId = Game_id +1,
+            whereis(Name) ! {choose_coin, {New_gameId}},
+            whereis(Random_Player) ! {choose_coin, {New_gameId}},
+            io:format("~p Name ~p Random Player inside Master~n", [Name, Random_Player]),
+            master_listener(MainMap,EligiblePlayersMap, New_gameId);
+
+        {player_coin, {Game_id, Response, PlayerName}}
+    end.
--- player.erl
+++ player.erl
@@ -1,135 +1,56 @@
 
-% % -module(player).
-% % -export([start/0, player_listener/3]).
-
-% % start() ->
-% %     spawn(player, player_listener, player_map).
-
-% % player_listener(Name, EligiblePlayers, MainMap) ->
-% %     receive
-% %         {message, {Name,  Initiate }} ->
-% %             io:format("~s received 'Initiate' message  ~n", [Name]),
-% %             % io:format("Second line \n"),
-% %             io:fwrite("~p received 'Initiate' message~n", [whereis(Name)]),
-% %             % io:format("Third line \n")
-% %             self() ! {request_to_play, Name},
-% %             io:fwrite("~p After message~n", [whereis(Name)]);
-
-% %         {request_to_play, Name} ->
-% %             io:format("~s REQUEST TO PLAY~n", [Name]),
-% %             Random_Ind = rand:uniform(length(EligiblePlayers)),
-% %             Random_Player = lists:nth(Random_Ind, EligiblePlayers)
-% %             % whereis(Random_Player) ! {accept, {Random_Player}}
-
-
-% %         % {accept, {Random_Player}} -> 
-% %         %     MapValue = maps:get(Random_Player, MainMap),
-% %         %     io:format("~s ACCEPT~n", [Name]),
-% %         %     if
-% %         %         MapValue > 0 ->
-% %         %             io:format("~s PLAYER~n", [Random_Player]);
-% %         %         true ->
-% %         %             io:format("Invalid player or MainMap value~n")
-% %         %     end,
-% %         %     player_listener(Name, EligiblePlayers, MainMap);
-% %         % _ ->
-% %         %     player_listener(Name, EligiblePlayers, MainMap)
-% %     end.
-
-
-% -module(player).
-% -export([start/0, player_listener/5]).
-
-% start() ->
-%     spawn(player, player_listener, []).
-
-% player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId) ->
-%     receive
-%         {message, {Name, "Initiate"}} ->
-%             % io:format("~s received 'Initiate' message~n", [Name]),
-%             % io:fwrite("~p received 'Initiate' message~n", [self()]),
-%             self() ! {request_to_play, Name},
-%             % io:fwrite("~p After message~n", [self()]),
-%             player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
-        
-%         {request_to_play, Name} ->
-%             io:format("~s REQUEST TO PLAY~n", [Name]),
-%             Random_Ind = rand:uniform(length(EligiblePlayers)),
-%             Random_Player = lists:nth(Random_Ind, EligiblePlayers),
-%             % io:fwrite("~p Current Name~n", [Name]),
-%             % io:fwrite("~p INDEXXXXXXXX~n", [Random_Ind]),
-%             % io:fwrite("~p Random Player Selected~n", [Random_Player]),
-%             whereis(Random_Player) ! {accept, {Random_Player}},
-%             player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
-        
-%         {accept, {Random_Player}} -> 
-%             io:fwrite("~p The Random PLayer sent~n", [Random_Player]),
-
-%             MapValue = maps:get(Random_Player, MainMap),
-%             io:format("~s ACCEPT~n", [Name]),
-%             if
-%                 MapValue > 0 ->
-%                     io:format("~s PLAYER~n", [Random_Player]);
-%                 true ->
-%                     io:format("Invalid player or MainMap value~n")
-%             end,
-%             player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
-        
-%         _ ->
-%             player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId)
-%     end.
 
 
 -module(player).
--export([ player_listener/5]).
+-export([player_listener/5]).
 
-% start() ->
-%     % spawn(player, player_listener, ["Alice", 100, ["Bob", "Charlie"], #{"Alice" => 100, "Bob" => 200, "Charlie" => 300}, self()]).
-
-player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId) ->
+player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId) ->
     receive
         {message, {Name, "Initiate"}} ->
             % io:format("~s received 'Initiate' message~n", [Name]),
             % io:fwrite("~p received 'Initiate' message~n", [self()]),
             self() ! {request_to_play, Name},
             % io:fwrite("~p After message~n", [self()]),
-            player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId);
+            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
         
         {request_to_play, Name} ->
-            % io:format("~s REQUEST TO PLAY~n", [Name]),
+            io:format("~s REQUEST TO PLAY~n", [Name]),
             Random_Ind = rand:uniform(length(EligiblePlayers)),
-            Random_Player = lists:nth(Random_Ind, EligiblePlayers),
-            io:fwrite("~p Name in Sequence~n", [Name]),
+            Player2 = lists:nth(Random_Ind, EligiblePlayers),
+            % io:fwrite("~p Current Name~n", [Name]),
             % io:fwrite("~p INDEXXXXXXXX~n", [Random_Ind]),
-            io:fwrite("~p Random Player Selected For Sequence~n", [Random_Player]),
-            self() ! {accept, {Random_Player}},
-            player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId); 
+            % io:fwrite("~p Random Player Selected~n", [Random_Player]),
+            %TODO: Check for 
+            whereis(Player2) ! {accept, {Name}},
+            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
         
-        {accept, {Random_Player}} -> 
-            % io:fwrite("~p The Random Player sent~n", [Random_Player]),
-            Name_MapValue = maps:get(Name, MainMap),
-            MapValue = maps:get(Random_Player, MainMap),
-            % io:format("~s ACCEPT~n", [Name]),
-            io:format("~p Name ~p PLAYER SCORE~n", [Name, Random_Player]),
-            % io:format("~p Random PLAYER SCORE~n", [maps:get(Random_Player, MainMap)]),
-            %  io:format("~p Name PLAYER SCORE~n", [Name]),
-            % io:format("~p Random PLAYER SCORE~n", [Random_Player]),
+        {accept, {Player1Name}} -> 
+            io:fwrite("~p The Random PLayer sent~n", [Player1Name]),
 
-            if
-                MapValue > 0 ->
-                    % io:format("~s Random Player~n", [Random_Player]),
-                    % io:fwrite("~p Current Name~n", [Name]),
-                    io:fwrite("In the Accept part", [Name]);
-
-                    % io:fwrite("~p Random Player Selected~n", [Random_Player]);
-
-                    % {create_game, {}}
-
+            NameValue = maps:get(Name, MainMap),
+            if 
+                NameValue > 0 ->
+                    whereis(Player1Name)! {message_player_2, Name};
                 true ->
-                    io:format("Credits less than or equal to zero~n")
+                    io:format("Invalid player or MainMap value~n")  
             end,
-            player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId);
+            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
+
+
+        {message_player_2, Player2Name} ->
+            io:fwrite("Inside message player 2\n"),
+            RandomValue = maps:get(Name, MainMap),
+            if
+                RandomValue > 0 ->
+                    MasterId ! {create_game,Name,Player2Name},
+                    io:format("PLayer 2 Value greater than 0\n");
+                true ->
+                    io:format("Invalid player or MainMap value~n")
+            end,
+            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
         
         _ ->
-            player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId)
+            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId)
     end.
+
+
