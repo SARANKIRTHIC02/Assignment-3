7955d707c7547cc17f628e15e76aa3683fa7a58a
SARANKIRTHIC02 <sarankirthic1@gmail.com>
2024-06-23 23:25:14 -0400

    working

--- game.erl
+++ game.erl
@@ -1,7 +1,5 @@
-
-
 -module(game).
--export([start/1, master_listener/7, create_eligible_players_map/2]).
+-export([start/1, master_listener/8, create_eligible_players_map/2]).
 
 start(Args) ->
     PlayerFile = lists:nth(1, Args),
@@ -10,27 +8,28 @@ start(Args) ->
 
     MainMap = maps:from_list(PlayerInfo),
     % io:fwrite("Main Map: ~p~n", [MainMap]),
+    io:format("Starting Game Log...~n ~n"),
 
     EligiblePlayersMap = create_eligible_players_map(MainMap, MainMap),
     lists:foreach(
         fun({Name, Credits}) ->
-            Pid = spawn(player, player_listener, [Name, Credits, maps:get(Name, EligiblePlayersMap), MainMap, self()]),
+            Pid = spawn(player, player_listener, [Name, Credits, maps:get(Name, EligiblePlayersMap),EligiblePlayersMap, MainMap, self()]),
             register(Name, Pid)
-            % io:format("Worker PID for ~p: ~p~n", [Name, whereis(Name)])
+            % io:format("Worker PID for ~p: ~p~n", [Name, whereis(Name)])   
         end,
         PlayerInfo
     ),
 
     lists:foreach(
         fun(Name) ->
-            io:fwrite("Current Player: ~p~n", [Name]),
-            Initiate = "Initiate",
+            % io:fwrite("Current Player: ~p~n", [Name]),,
             Current = whereis(Name),
-            Current ! {message, {Name, Initiate}}
+            Current ! {message, {Name, initiate}}
         end,
         maps:keys(MainMap)
     ),
-    master_listener(MainMap, EligiblePlayersMap, 1, #{}, #{}, #{}, #{}).
+    timer:sleep(200),
+    master_listener(MainMap, EligiblePlayersMap, 1, #{}, #{}, #{}, #{},false).
 
 create_eligible_players_map(MainMap, EligiblePlayersMap) ->
     lists:foldl(
@@ -43,8 +42,7 @@ create_eligible_players_map(MainMap, EligiblePlayersMap) ->
     )
 .
 
-
-master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, Player_One_Map, Player_Two_Map, Player_Details_Map) ->
+master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, Player_One_Map, Player_Two_Map, Player_Details_Map,Declared) ->
     receive
         {create_game, Name, Player2Name} ->
             New_gameId = Game_id + 1,
@@ -52,16 +50,17 @@ master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, Player_One_Ma
             UpdatedPlayersMap = maps:put(Temp_id, [Name, Player2Name], Players_Map),
             whereis(Name) ! {choose_move, {Temp_id, Name}}, 
             whereis(Player2Name) ! {choose_move, {Temp_id, Player2Name}}, 
-            % io:format("Created game with ID ~p~n", [Temp_id]),
+            UpdatedPlayersMap = maps:put(Temp_id, [Name, Player2Name], Players_Map),
+            io:format("+ [~p] new game for ~p -> ~p ~n", [Temp_id,Name,Player2Name]),
             % io:format("Updated Players Map: ~p~n", [UpdatedPlayersMap]),
-            master_listener(MainMap, EligiblePlayersMap, New_gameId, UpdatedPlayersMap, Player_One_Map, Player_Two_Map, Player_Details_Map);
+            master_listener(MainMap, EligiblePlayersMap, New_gameId, UpdatedPlayersMap, Player_One_Map, Player_Two_Map, Player_Details_Map,Declared);
 
     {player_response, {GameId, Move, PlayerName}} ->
-    % io:format("Received Move for Game ID ~p: ~p from ~p~n", [GameId, Move, PlayerName]),
-    Players_List = maps:get(GameId, Players_Map),
-    % io:format("Players list: ~p ~n",[Players_List]),
-    % io:format("Current Players in Game ID ~p: ~p~n", [GameId, Players_List]),
+        io:format("~p sssssssssssssssssssssssss ~n",[GameId]),
+        % io:format("~p sssssssssssssssssssssssss ~n",[Players_Map]),
+        io:format("~p sssssssssssssssssssssssss ~n",[PlayerName]),
 
+    Players_List = maps:get(GameId, Players_Map),
     UpdatedPlayerOneMap = case {hd(Players_List), PlayerName} of
         {PlayerName, _} ->
             OldMovesOne = maps:get(GameId, Player_One_Map, []),
@@ -70,8 +69,6 @@ master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, Player_One_Ma
         _ ->
             Player_One_Map
     end,
-    % io:format("Updated Player One Map: ~p~n", [UpdatedPlayerOneMap]),
-
     UpdatedPlayerTwoMap = case {lists:last(Players_List), PlayerName} of
         {PlayerName, _} ->
             OldMovesTwo = maps:get(GameId, Player_Two_Map, []),
@@ -81,71 +78,93 @@ master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, Player_One_Ma
             Player_Two_Map
     end,
 
-    
+    % io:format("~p playerones ~n",[UpdatedPlayerOneMap]),
+    % io:format("~p playertrwo ~n",[UpdatedPlayerTwoMap]),
+
     PlayerOneRematch = hd(Players_List),
     PlayerTwoRematch =  lists:last(Players_List),
-    io:format("~p ~n",[PlayerOneRematch]),
-    io:format("~p ~n",[PlayerTwoRematch]),
-    % io:format("Updated Player Two Map: ~p~n", [UpdatedPlayerTwoMap]),
-    % Get the list of moves for Player One
+    % io:format("~p ~n",[PlayerOneRematch]),
+    % io:format("~p ~n",[PlayerTwoRematch]),
     PlayerOneMoves = maps:get(GameId, UpdatedPlayerOneMap, []),
     % io:format("Player One Moves: ~p~n", [PlayerOneMoves]),
     % Get the list of moves for Player Two
     PlayerTwoMoves = maps:get(GameId, UpdatedPlayerTwoMap, []),
     % io:format("Player Two Moves: ~p~n", [PlayerTwoMoves]),
-
     
     if 
         length(PlayerOneMoves) == length(PlayerTwoMoves) ->
-            % io:format("Fuck yes ~n"),
-            % io:format("Player One Moves: ~p~n", [PlayerOneMoves]),
-            % io:format("Player Two Moves: ~p~n", [PlayerTwoMoves]),
-            % io:format("~p ~n",[PlayerOneMoves]),
-            % io:format("~p ~n",[PlayerTwoMoves]),
             OneLastMove = lists:last(PlayerOneMoves),
             TwoLastMove = lists:last(PlayerTwoMoves),
-            % io:format("Last Player One Move: ~p~n", [OneLastMove]),
-            % io:format("Last Player Two Move: ~p~n", [TwoLastMove]),
-
             if 
                 (OneLastMove == 1 andalso TwoLastMove == 3) orelse (OneLastMove == 3 andalso TwoLastMove == 2) orelse (OneLastMove == 2 andalso TwoLastMove == 1) ->  
-                io:format("PLayer 1 Wins ~n"),
+                % TwoPLayerCredit = maps:get(PlayerTwoRematch,MainMap),
+                % io:format("~p ~n",[MainMap]),
                 Current_Player_Two_Credits = maps:get(PlayerTwoRematch,MainMap)-1,
-                maps:put(GameId,Current_Player_Two_Credits,Player_Two_Map),
-                io:format("PLayer 1 Wins ~p ~n",[Current_Player_Two_Credits]),
+                Temp_Map = maps:put(PlayerTwoRematch,Current_Player_Two_Credits,MainMap),
+                io:format("~p ~n player@Creditssssssss ~p",[Current_Player_Two_Credits,PlayerTwoRematch]),
 
+                io:format("$ (~p) ~p:~p -> ~p:~p = ~p loses [~p credits left]~n",[GameId,PlayerOneRematch,OneLastMove,PlayerTwoRematch,TwoLastMove,PlayerTwoRematch,Current_Player_Two_Credits]),
+                % io:format("~p ~n",[Current_Player_Two_Credits]),
+                % io:format("~p ~n",[Temp_Map]),
 
-                if
-                    Current_Player_Two_Credits == 0 ->
-                    whereis(PlayerTwoRematch) ! {disqualify_player, PlayerTwoRematch};
+                if 
+                   Current_Player_Two_Credits ==0 ->
+                   whereis(PlayerTwoRematch) ! {disqualify_player, PlayerTwoRematch},
+                %    io:format("ascddvfevdfvsfbgbfgdbgbbgldvcnf ~p ~n",[Temp_Map]),
+                   FilteredMap = maps:filter(fun(_Key, Value) -> Value > 0 end, Temp_Map),
+                %    io:foxrmat(" asdfnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn~p ~n",[FilteredMap]),
+                    Size = maps:size(FilteredMap),
+                    % io:format(" ~p Size ~n",[Size]),
+                   if 
+                        Size == 1 ->
+                            io:format("We have a Winner --------------------------------------~p ~n",[FilteredMap]);
+                        true->
+                            master_listener(Temp_Map, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,Declared)
+                    end;
+                   
+                   true ->
+                        master_listener(Temp_Map, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,Declared)
 
-                    true ->
-                        io:format("Game Draw ~n")
-                end;    
+                end;
 
                 (OneLastMove == 3 andalso TwoLastMove == 1) orelse (OneLastMove == 2 andalso TwoLastMove == 3) orelse (OneLastMove == 1 andalso TwoLastMove == 2) ->  
-                io:format("PLayer 2 Wins ~n");
+                % io:format("PLayer 2 Wins ~p ~n",[PlayerTwoRematch]),
+                io:format("~p ~n",[MainMap]),
+                Current_Player_One_Credits = maps:get(PlayerOneRematch,MainMap)-1,
+                Temp_Map_Two = maps:put(PlayerOneRematch,Current_Player_One_Credits,MainMap),
+                                io:format("PLayer Lost ~p ~n",[Current_Player_One_Credits]),
+                % io:format("~p ~n",[Current_Player_One_Credits]),
+                io:format("$ (~p) ~p:~p -> ~p:~p = ~p loses [~p credits left]~n",[GameId,PlayerTwoRematch,TwoLastMove,PlayerOneRematch,OneLastMove,PlayerOneRematch,Current_Player_One_Credits]),
+                % io:format("~p ~n",[Temp_Map_Two]),
 
+                if 
+                   Current_Player_One_Credits ==0 ->
+                        whereis(PlayerOneRematch) ! {disqualify_player, PlayerOneRematch},
+                        Filtered_Map_Two = maps:filter(fun(_Key, Value) -> Value > 0 end, Temp_Map_Two),
+                        Size_Two = maps:size(Filtered_Map_Two),
+                %    io:format(" ~p Size ~n",[Size_Two]),
+                        if 
+                            Size_Two == 1 ->
+                                io:format("We have a Winner  222222222222222222222222222--------------------------------------~p ~n",[Filtered_Map_Two]);
+
+                            true->
+                                master_listener(Temp_Map_Two, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,true)
+                        end;
+
+
+                   true ->
+                    master_listener(Temp_Map_Two, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,true)
+                end;
+            
                 (OneLastMove == 1 andalso TwoLastMove == 1) orelse (OneLastMove == 2 andalso TwoLastMove == 2) orelse (OneLastMove == 3 andalso TwoLastMove == 3) ->  
-                io:format("Draw ~n"),
-                whereis(PlayerOneRematch) ! {choose_move, {GameId,PlayerOneRematch }},
-                whereis(PlayerTwoRematch) ! {choose_move, {GameId,PlayerTwoRematch}};
-
-            
-            true ->
-                io:format("Game Draw ~n")
+                    io:format(" {~p} Game Draw ~n",[GameId]),
+                    whereis(PlayerOneRematch) ! {choose_move, {GameId,PlayerOneRematch }},
+                    whereis(PlayerTwoRematch) ! {choose_move, {GameId,PlayerTwoRematch}},
+                    master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,true);
+                true ->
+                    master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,true)
             end;   
-            % io:format("Player One Moves: ~p~n", [lists:last(PlayerOneMoves)]),
-
-
-
         true ->
-            ok
-            
-    end,
-    % io:format("PLayer oneeeeeee movessssss ~p ~n",[lists:last(PlayerOneMoves)]),
-    % io:format("Player twoooooo movessssssss ~p ~n",[lists:last(PlayerTwoMoves)]),
-    master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map)
+            master_listener(MainMap, EligiblePlayersMap, Game_id, Players_Map, UpdatedPlayerOneMap, UpdatedPlayerTwoMap, Player_Details_Map,true)  
+    end
 end.
-
-
--- player.erl
+++ player.erl
@@ -1,76 +1,64 @@
 
-
-
 -module(player).
--export([player_listener/5]).
+-export([player_listener/6]).
 
-player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId) ->
+player_listener(Name, Credits, EligiblePlayers,EligiblePlayersMap, MainMap, MasterId) ->
     receive
-        {message, {Name, "Initiate"}} ->
-            % io:format("~s received 'Initiate' message~n", [Name]),
-            % io:fwrite("~p received 'Initiate' message~n", [self()]),
+        {message, {Name, initiate}} ->
             self() ! {request_to_play, Name},
-            % io:fwrite("~p After message~n", [self()]),
-            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
-        
+            player_listener(Name, Credits, EligiblePlayers,EligiblePlayersMap, MainMap, MasterId);
+
         {request_to_play, Name} ->
-            % io:format("~s REQUEST TO PLAY~n", [Name]),
+            NamePLayerCredits = maps:get(Name,MainMap),
+            if 
+            NamePLayerCredits > 0 ->
             Random_Ind = rand:uniform(length(EligiblePlayers)),
             Player2 = lists:nth(Random_Ind, EligiblePlayers),
-            % io:fwrite("~p Current Name~n", [Name]),
-            % io:fwrite("~p INDEXXXXXXXX~n", [Random_Ind]),
-            % io:fwrite("~p Random Player Selected~n", [Random_Player]),
-            %TODO: Check for 
             whereis(Player2) ! {accept, {Name}},
-            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
-        
-        {accept, {Player1Name}} -> 
+            timer:send_after(100, self(),  {request_to_play, Name});
+            true ->
+                ok
 
-            % io:fwrite("~p The Random PLayer sent~n", [Player1Name]),
-
-            NameValue = maps:get(Name, MainMap),
-            if 
-                NameValue > 0 ->
-                    whereis(Player1Name)! {message_player_2, Name};
-                true ->
-                    % io:format("Invalid player or MainMap value~n") 
-                    ok 
             end,
-            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
+            player_listener(Name, Credits, EligiblePlayers, EligiblePlayersMap,MainMap, MasterId);
 
-            % Send back to Player 1
+        {accept, {Player1Name}} ->
+            NameValue = maps:get(Name, MainMap),
+            if
+                NameValue > 0 ->
+                    whereis(Player1Name) ! {message_player_2, Name};
+                true ->
+                    ok
+            end,
+            player_listener(Name, Credits, EligiblePlayers, EligiblePlayersMap,MainMap, MasterId);
+
+        {player_2_accpeted, Player2Name} ->
+            MasterId ! {create_game, Name, Player2Name},
+            player_listener(Name, Credits, EligiblePlayers, EligiblePlayersMap,MainMap, MasterId);
+        
+        {player_2_rejected, Player2Name}->
+            List_To_Change = maps:get(Name,EligiblePlayers),
+            NewList = lists:delete(Player2Name,List_To_Change),
+            Add_To_Eligible_Map = maps:put(Name,NewList,EligiblePlayers),
+            player_listener(Name, Credits, EligiblePlayers, Add_To_Eligible_Map,MainMap, MasterId);
+
         {message_player_2, Player2Name} ->
-            % io:fwrite("Inside message player 2\n"),
             RandomValue = maps:get(Name, MainMap),
             if
                 RandomValue > 0 ->
-                    MasterId ! {create_game,Name,Player2Name};
-                    % io:format("~p Name ~p Random Player inside Create~n", [Name, Player2Name]);
-
+                    whereis(Name) ! {player_2_accpeted, Player2Name};
                 true ->
-                    % io:format("Invalid player or MainMap value~n")
-                    ok
+                    ok                  
             end,
-            player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId);
+            player_listener(Name, Credits, EligiblePlayers, EligiblePlayersMap,MainMap, MasterId);
 
         {choose_move, {Temp_id, PlayerName}} ->
             Move = rand:uniform(3),
-            % io:format("Reached The Choose Move ~n"),
             MasterId ! {player_response, {Temp_id, Move, PlayerName}},
-            % io:format("Move Sent To Master ~p ~n",[Move]),
+            player_listener(Name, Credits, EligiblePlayers,EligiblePlayersMap ,MainMap, MasterId);
 
-            player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId);
-        
-    
-        % _->
-        %     player_listener(Name,Credits, EligiblePlayers, MainMap,MasterId)
-        
-        {disqualify_player , PlayerNameDisqualify} ->
-            io:format("~p ~n",[Credits-1]),
-            
-            player_listener(Name, Credits, EligiblePlayers, MainMap, MasterId)
-
-
-        
-        end.
-
+        {disqualify_player, PlayerNameDisqualify} ->
+            % io:format(" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ~p ~n",[PlayerNameDisqualify]),
+            Edited_Main_Map = maps:put(PlayerNameDisqualify,0,MainMap),
+            player_listener(Name, Credits, EligiblePlayers,EligiblePlayersMap ,Edited_Main_Map, MasterId)
+    end.
